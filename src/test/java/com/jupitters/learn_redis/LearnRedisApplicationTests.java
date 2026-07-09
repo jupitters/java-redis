626f459f15293b0662659fa55272ce7f380440c6
jupitters <sampaiobale@gmail.com>
2026-07-09 11:20:18 -0300

    init

---
+++ src/test/java/com/jupitters/learn_redis/LearnRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jupitters.learn_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
