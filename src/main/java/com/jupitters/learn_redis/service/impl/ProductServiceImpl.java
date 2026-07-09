3bc9c06205fe1702d01eb8a499ff3076547f7e8b
jupitters <sampaiobale@gmail.com>
2026-07-09 11:42:50 -0300

    product service

--- src/main/java/com/jupitters/learn_redis/service/impl/ProductServiceImpl.java
+++ src/main/java/com/jupitters/learn_redis/service/impl/ProductServiceImpl.java
@@ -1,9 +1,19 @@
 package com.jupitters.learn_redis.service.impl;
 
+import com.jupitters.learn_redis.entity.Product;
+import com.jupitters.learn_redis.repository.ProductRepository;
 import com.jupitters.learn_redis.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Service
-public class ProductServiceImpl extends ProductService {
+import java.util.List;
 
+@Service
+@RequiredArgsConstructor
+public class ProductServiceImpl extends ProductService {
+    private final ProductRepository productRepository;
+
+    public List<Product> getAllProducts(){
+        return productRepository.findAll();
+    }
 }
