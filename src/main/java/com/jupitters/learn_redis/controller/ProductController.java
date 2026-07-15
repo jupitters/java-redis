0e1c07c6a0da716e9f09975a6b44359c7ce76a8a
jupitters <sampaiobale@gmail.com>
2026-07-15 11:17:59 -0300

    product controller all products

---
+++ src/main/java/com/jupitters/learn_redis/controller/ProductController.java
@@ -0,0 +1,23 @@
+package com.jupitters.learn_redis.controller;
+
+import com.jupitters.learn_redis.entity.Product;
+import com.jupitters.learn_redis.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+}
