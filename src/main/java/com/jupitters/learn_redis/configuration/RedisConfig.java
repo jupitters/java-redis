41ddc2d014f398b19b4b500e8e80a0442cdd128b
jupitters <sampaiobale@gmail.com>
2026-07-17 12:45:54 -0300

    dependencies

--- src/main/java/com/jupitters/learn_redis/configuration/RedisConfig.java
+++ src/main/java/com/jupitters/learn_redis/configuration/RedisConfig.java
@@ -1,7 +1,15 @@
 package com.jupitters.learn_redis.configuration;
 
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
 public class RedisConfig {
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+
+    }
 }
