25d22289b89b47b863df9ea713afa8eea5312a19
jupitters <sampaiobale@gmail.com>
2026-07-17 12:57:56 -0300

    config

--- src/main/java/com/jupitters/learn_redis/configuration/RedisConfig.java
+++ src/main/java/com/jupitters/learn_redis/configuration/RedisConfig.java
@@ -1,15 +1,24 @@
 package com.jupitters.learn_redis.configuration;
 
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 @Configuration
 public class RedisConfig {
     public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
+        mapper.activateDefaultTyping(
+                LaissezFaireSubTypeValidator.instance,
+                ObjectMapper.DefaultTyping.NON_FINAL
+        );
     }
 }
